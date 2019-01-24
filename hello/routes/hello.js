f35f3e45163c474df712cff047b2e27513e51e4a
tiebin Zhu <tiz125@ucsd.edu>
2019-01-24 11:05:30 -0800

    hello.js changed

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -5,6 +5,9 @@
 
 exports.view = function(req, res){
   var nameToShow = req.params.userName;
+  if (nameToShow === undefined){
+  	nameToShow = 'World';
+  }
   console.log("name is " + nameToShow);
   res.render('index', {
   	'name': nameToShow,
